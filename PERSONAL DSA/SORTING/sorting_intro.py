9cc970aa34e13f5bf4705a367bb5f180f3692e25
bhowmikvij <bhowmik.vij14@gmail.com>
2026-07-14 22:54:57 +0530

    Heap concept is learnt today

--- PERSONAL DSA/SORTING/sorting_intro.py
+++ PERSONAL DSA/SORTING/sorting_intro.py
@@ -15,3 +15,73 @@
 
 # Bubble Sort=> TIME COMPLEXITY=> O(n^2)
 # Selection Sort=> TIME COMPLEXITY=> O(n^2)
+
+# INTERVIEW IMPORTANT QUESTION
+# Given a scenario, array is sorted or almost sorted so which sorting algorithm is preferrable and why??
+# Insertion Sort=> O(n)
+
+# Which sorting algo is preffered when the array is highly unsorted?
+# Quick Sort
+
+# --------------------------------------------------------------------------------------------------------------------------
+
+#                     Heap Data Structure
+#                             /\
+#                            /  \
+#                           /    \
+#                          /      \
+#                      Maxheap   Minheap
+
+# Tree Data Structure--> Non-Linear Data Structure
+
+# Binary Tree
+
+# Top Element--> Root Node
+# Middle Element--> Internal Node
+# Last Element--> Leaf Node
+
+# IMPORTANT INFORM/ INTERVIEW IMPORTANT QUESTION
+# Full Binary Tree(Perfect Binary Tree) VS Almost Complete Binary Tree VS Complete Binary tree?
+
+# Common for above three different binary tree
+# 1. Insertion of Node--> (left to right)
+# 2. Upper level nodes will be filled up before coming to the lower level
+
+
+# Heap--> Complete Binary Tree 
+# time complexity: O(1)
+# 1.Minheap--> Parent node
+#              Data < Child node
+
+# 2.Maxheap--> Parent node
+#              Data > Child node
+
+# VERY INMPORTANT INFORMATION ABOUT STORING DATA OF BINARY TREE INTO ARRAY
+#                        (10) 0
+#                         /\
+#                        /  \
+#           2*i + 1 = 1 /    \ 2*i + 2 = 2
+#                    (20)    (30)
+#                     /\
+#                    /  \
+#       2*i + 1 = 3 /    \ 2*i + 1 = 4
+#                 (40)   (50)
+
+#              [10, 20, 30, 40, 50]
+
+# ------------------------------------------------
+
+#                        (10) 0
+#                         /
+#           2*i + 1 = 1  /    
+#                    (20)    
+#                     / 
+#       2*i + 1 = 3  /    
+#                 (40)   
+#                  /  
+#     2*i + 1 = 7 /    
+#              (50)
+
+# [10, 20,  , 40,  ,  ,  , 50]
+# There is very wastage of space in array so that's why we use LINKED LIST
+
